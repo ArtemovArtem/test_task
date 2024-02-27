779298f88465067454c224947b52a20f0cba9a19
aaid <davaani1983@mail.ru>
2024-02-27 13:32:52 +0300

    jj

--- myapp/templatetags/custom_tags.py
+++ myapp/templatetags/custom_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.inclusion_tag('myapp/menu.html')
 def draw_menu_templates(menu_name: str = None, menu_item: str = None):
 
-    def get_menu(menu_item: str = None, submenu: list = None):
+    def get_menu(menu_item: str = None):
         menu = list(items.filter(parent=None)) if menu_item is None else list(items.filter(parent__name=menu_item))
 
         return menu
